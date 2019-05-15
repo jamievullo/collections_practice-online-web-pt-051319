960b8f123799b741016a686ff0189e1a9f3cd5c7
Jamie Vullo <jvullo@protonmail.com>
2019-05-15 13:45:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,7 +8,7 @@ end
 
 def sort_array_char_count(array)
   new_array = []
-  array.each {|count| count == array.size}
+  array.each {|count| array == array.size}
   new_array << count 
   new_array.sort 
 end 
