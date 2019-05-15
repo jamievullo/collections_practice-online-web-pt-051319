41b23ca2626428e13799cf94464751a682a33a45
Jamie Vullo <jvullo@protonmail.com>
2019-05-15 14:54:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,7 @@ def sort_array_char_count(array)
   new_array = []
   array.each do |element| 
   element.length
-  new_array << element.length
+  new_array << element.length.to_s
 end 
 new_array.sort
 end
